9fe71a589e87b17ffdd8130fd69d34021cbd1216
Nathanael Joshua <nathanael.soesapto@binus.ac.id>
2026-03-17 14:15:12 +0700

    route for WinScr and lvlMap

--- frontend/src/app/router.tsx
+++ frontend/src/app/router.tsx
@@ -2,15 +2,18 @@ import { BrowserRouter, Routes, Route } from "react-router-dom"
 
 import Battle from "../pages/battle"
 import LandingPage from "../pages/landingpage"
+import Map from "../pages/map"
+import WinScreen from "../pages/winscreen"
 
 export default function AppRouter() {
   return (
     <BrowserRouter>
       <Routes>
-
+        <Route path="/" element={<LandingPage />} />
         <Route path="/Battle" element={<Battle />} />
         <Route path="/LandingPage" element={<LandingPage />} />
-
+        <Route path="/Map" element={<Map />} />
+        <Route path="/WinScreen" element={<WinScreen />} />
   
 
       </Routes>
